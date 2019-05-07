75abf90c3a2310f5ae37053d003bd057352c04e1
Mark Berney <berneydwmark@gmail.com>
2019-05-07 23:20:14 +1200

    AnimalProcessor completed and VetGUI making headway

--- src/question3/AnimalProcessor.java
+++ src/question3/AnimalProcessor.java
@@ -2,52 +2,117 @@ package question3;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.LinkedList;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class AnimalProcessor {
-    private LinkedList<AnimalPatient> waitlist;
-
-    public AnimalProcessor() {
-
-    }
+    final private String WIN_XML_PATH = "C:\\Users\\berne\\IdeaProjects\\DSAAssignment2\\src\\question3\\animals.xml";
+    final private String MAC_XML_PATH = "";
+    private LinkedList<AnimalPatient> waitlist = new LinkedList<>();
 
     public void addAnimal(AnimalPatient animal) {
-        waitlist.add(animal);
+        waitlist.addFirst(animal);
     }
 
     public AnimalPatient getNextAnimal() {
-        return null;
+        return waitlist.getFirst();
     }
 
     public AnimalPatient releaseAnimal() {
-        return waitlist.remove();
+        return waitlist.removeFirst();
+    }
+
+    public LinkedList<AnimalPatient> getWaitlist() {
+        return waitlist;
     }
 
     public int animalsLeftToProcess() {
-        return 0;
+        return waitlist.size();
     }
 
     public void loadAnimalsFromXML(Document document) {
+        String name;
+        String species;
 
+        int day, month, year;
+        int time, hours, minutes;
+        Date lastVisited;
+
+        try {
+            Node rootElement = document.getFirstChild();
+
+            if (rootElement != null) {
+                NodeList children = document.getElementsByTagName("animal");
+
+                for (int i = 0; i < children.getLength(); i++) {
+                    Node child = children.item(i);
+
+                    if (child.getNodeType() == Node.ELEMENT_NODE) {
+                        Element animalElement = (Element)child;
+
+                        NodeList nameList = animalElement.getElementsByTagName("name");
+                        NodeList textNameList = ((Element)nameList.item(0)).getChildNodes();
+                        name = (textNameList.item(0).getNodeValue().trim());
+
+                        NodeList speciesList = animalElement.getElementsByTagName("species");
+                        NodeList textSpeciesList = ((Element)speciesList.item(0)).getChildNodes();
+                        species = textSpeciesList.item(0).getNodeValue().trim();
+
+                        NodeList dayList = animalElement.getElementsByTagName("day");
+                        NodeList textDayList = ((Element)dayList.item(0)).getChildNodes();
+                        day = Integer.parseInt(textDayList.item(0).getNodeValue().trim());
+
+                        NodeList monthList = animalElement.getElementsByTagName("month");
+                        NodeList textMonthList = ((Element)monthList.item(0)).getChildNodes();
+                        month = Integer.parseInt(textMonthList.item(0).getNodeValue().trim()) - 1;
+
+                        NodeList yearList = animalElement.getElementsByTagName("year");
+                        NodeList textYearList = ((Element)yearList.item(0)).getChildNodes();
+                        year = Integer.parseInt(textYearList.item(0).getNodeValue().trim()) - 1900;
+
+                        NodeList timeList = animalElement.getElementsByTagName("time");
+                        NodeList textTimeList = ((Element)timeList.item(0)).getChildNodes();
+                        time = Integer.parseInt(textTimeList.item(0).getNodeValue().trim());
+                        hours = time / 100;
+                        minutes = time % 100;
+                        lastVisited = new Date(year, month, day, hours, minutes);
+
+                        waitlist.addLast(new AnimalPatient(species, name, lastVisited));
+                        waitlist.getLast().setPriority(5);
+                    }
+                }
+            } else System.out.println("ROOT = NULL");
+        } catch (NegativeArraySizeException e) {
+            e.getMessage();
+        }
+    }
+
+    public void loadDocument() {
+        File vetPatients = new File(WIN_XML_PATH);
+
+        try {
+            DocumentBuilderFactory docBuildFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuild = docBuildFactory.newDocumentBuilder();
+            Document patientFiles = docBuild.parse(vetPatients);
+            patientFiles.getDocumentElement().normalize();
+            loadAnimalsFromXML(patientFiles);
+        } catch(IOException | SAXException | ParserConfigurationException e) {
+            e.getMessage();
+        }
     }
 
     public static void main(String[] args) {
         AnimalProcessor ap = new AnimalProcessor();
-        Document patientFiles = null;
+        ap.loadDocument();
 
-
-        try {
-            File vetPatients = new File("question3/animals.xml");
-            DocumentBuilderFactory docBuildFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuild = docBuildFactory.newDocumentBuilder();
-            patientFiles = docBuild.parse(vetPatients);
-            patientFiles.getDocumentElement ().normalize ();
-        } catch(Exception e) {
-            e.getMessage();
-        }
-
-        ap.loadAnimalsFromXML(patientFiles);
+        System.out.println(ap.waitlist);
     }
 }
--- src/question3/VetGUI.java
+++ src/question3/VetGUI.java
@@ -6,24 +6,69 @@ import javax.swing.*;
 import java.awt.*;
 
 public class VetGUI {
+    final private AnimalProcessor processor = new AnimalProcessor();
+
+    private JFrame mainFrame;
+    private JPanel mainPanel;
+    private JPanel listPanel;
+    private JList patientList;
+
+    private DefaultListModel animalPatients;
+
     public VetGUI() {
         GUIInit();
+
+        processor.loadDocument();
+        animalPatients = new DefaultListModel();
+        for (int i = 0; i < processor.getWaitlist().size(); i++) {
+            animalPatients.add(i, processor.getWaitlist().get(i).getName());
+        }
+
+        UIDesign();
+
+        mainFrame.setVisible(true);
     }
 
     public void GUIInit() {
-        JFrame frame = new JFrame("Mark's Vet");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane();
-        frame.setPreferredSize(new Dimension(600, 600));
+        mainFrame = new JFrame("Mark's Vet");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.getContentPane();
+        mainFrame.setPreferredSize(new Dimension(800, 600));
 
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension dimension = toolkit.getScreenSize();
         int screenHeight = dimension.height;
         int screenWidth = dimension.width;
-        frame.pack();
+        mainFrame.pack();
 
-        frame.setLocation(new Point((screenWidth / 2) - (frame.getWidth() / 2), (screenHeight / 2) - (frame.getHeight() / 2)));
-        frame.setVisible(true);
+        mainFrame.setLocation(new Point((screenWidth / 2) - (mainFrame.getWidth() / 2), (screenHeight / 2) - (mainFrame.getHeight() / 2)));
+    }
+
+    public void UIDesign() {
+        mainPanel = new JPanel(new BorderLayout());
+        mainFrame.add(mainPanel);
+
+        listPanel = new JPanel();
+        listPanel.setPreferredSize(new Dimension(200, 600));
+        listPanel.setBackground(Color.CYAN);
+        listPanel.setLayout(new BorderLayout());
+        mainPanel.add(listPanel, BorderLayout.WEST);
+
+        patientList = new JList(animalPatients);
+        patientList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        patientList.setLayoutOrientation(JList.VERTICAL);
+        patientList.setVisibleRowCount(-1);
+        listPanel.add(patientList, BorderLayout.CENTER);
+    }
+
+    public void checkPatientList(AnimalProcessor processor) {
+        if (processor != null) {
+            processor.loadDocument();
+
+            if (processor.getWaitlist() != null) {
+
+            }
+        }
     }
 
     public static void main(String[] args) {
