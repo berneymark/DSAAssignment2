16ce9c5daf66c454ab4e8a5b252ab67a5c2b7cdf
Mark Berney <berneydwmark@gmail.com>
2019-05-14 14:12:04 +1200

    Update GUITree.java

--- src/question2/GUITree.java
+++ src/question2/GUITree.java
@@ -1,41 +1,110 @@
 package question2;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class GUITree {
-    private JFrame mainFrame;
-    private JPanel controlPanel;
-    private JButton add, remove;
-
-    public GUITree() {
-        GUIInit();
-
-        mainFrame.setVisible(true);
-    }
-
-    public void GUIInit() {
-        mainFrame = new JFrame("GUI Tree");
+	private BinarySearchTree bst;
+	
+	private JFrame mainFrame;
+	private JPanel mainPanel, drawPanel, controlPanel;
+	private JButton addButton, removeButton;
+	private JButton leftRotationButton, rightRotationButton;
+	private JButton levelOrderButton, inOrderButton;
+	
+	public GUITree() {
+		bst = new BinarySearchTree();
+		GUIInit();
+		setDrawPanel();
+		setControlPanel();
+		mainFrame.setVisible(true);
+	}
+	
+	public void GUIInit() {
+		mainFrame = new JFrame("GUI Tree");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.getContentPane();
         mainFrame.setPreferredSize(new Dimension(600, 600));
-        mainFrame.setLayout(new BorderLayout());
 
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension dimension = toolkit.getScreenSize();
         int screenHeight = dimension.height;
         int screenWidth = dimension.width;
         mainFrame.pack();
-
         mainFrame.setLocation(new Point((screenWidth / 2) - (mainFrame.getWidth() / 2), (screenHeight / 2) - (mainFrame.getHeight() / 2)));
-    }
 
-    public void controlPanel() {
-        controlPanel = new JPanel();
-        //controlPanel.setLayout(BorderLayout.SOUTH);
-    }
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainFrame.add(mainPanel);
+	}
+	
+	public void setDrawPanel() {
+		drawPanel = new JPanel();
+		drawPanel.setBackground(Color.YELLOW);
+		
+		mainPanel.add(drawPanel, BorderLayout.CENTER);
+	}
+	
+	public void setControlPanel() {
+		controlPanel = new JPanel();
+		
+		addButton = new JButton("Add");
+		addButton.addActionListener(new ActionListeners());
+		controlPanel.add(addButton);
+		
+		removeButton = new JButton("Remove");
+		removeButton.addActionListener(new ActionListeners());
+		controlPanel.add(removeButton);
+		
+		leftRotationButton = new JButton("Left Rotation");
+		leftRotationButton.addActionListener(new ActionListeners());
+		controlPanel.add(leftRotationButton);
+		
+		rightRotationButton = new JButton("Right Rotation");
+		rightRotationButton.addActionListener(new ActionListeners());
+		controlPanel.add(rightRotationButton);
+		
+		mainPanel.add(controlPanel, BorderLayout.SOUTH);
+	}
+	
+	public class ActionListeners implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == addButton) {
+				String input = JOptionPane.showInputDialog("Add Text");
+				
+				if (input != null) {
+					JOptionPane.showMessageDialog(null, input);
 
-    public static void main(String[] args) {
-        GUITree main = new GUITree();
-    }
+					bst.add(input);
+					bst.drawTree(g, 600);
+				} else if (input.equals("")) {
+					JOptionPane.showMessageDialog(null, "You have submitted empty text.");
+				} else {
+					JOptionPane.showMessageDialog(null, "You have cancelled submission.");
+				}
+			} else if (e.getSource() == removeButton) {
+				JOptionPane.showMessageDialog(null, "You have removed node.");
+			} else if (e.getSource() == leftRotationButton) {
+				bst.leftRotation();
+			} else if (e.getSource() == rightRotationButton) {
+				bst.rightRotation();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		GUITree main = new GUITree();
+	}
 }
